8f442c2dd22f974bf1378b242fc1dfeeaa1b6453
Elmer de Looff <elmer.delooff@gmail.com>
2022-12-28 18:20:13 +0000

    Removes 'using' of std:: and qualifies use of stdlib.

--- composer.cpp
+++ composer.cpp
@@ -7,23 +7,21 @@
 #include <unordered_map>
 #include <vector>
 
-using namespace std;
-
 class regex_iter_match {
   // A range-for compatible regular expression iterator.
  public:
-  typedef regex_iterator<string::const_iterator> iterator;
+  typedef std::regex_iterator<std::string::const_iterator> iterator;
 
-  regex_iter_match(const string& s, const string& expr)
+  regex_iter_match(const std::string& s, const std::string& expr)
       : _expr(expr), _begin(s.begin(), s.end(), _expr), _end() {}
-  regex_iter_match(const ssub_match& m, const regex& expr)
+  regex_iter_match(const std::ssub_match& m, const std::regex& expr)
       : _expr(expr), _begin(m.first, m.second, _expr), _end() {}
 
   iterator begin() const { return _begin; }
   iterator end() const { return _end; }
 
  private:
-  regex _expr;
+  std::regex _expr;
   iterator _begin;
   iterator _end;
 };
@@ -34,10 +32,12 @@ class Stem {
   const char size;
 
   Stem(const char species, const char size) : species(species), size(size) {
-    if (species < 'a' || 'z' < species)
-      throw invalid_argument("Species not in range a-z: " + string(1, species));
+    if (species < 'a' || 'z' < species) {
+      auto err_msg = std::string("Species not in range a-z: ") + species;
+      throw std::invalid_argument(err_msg);
+    }
     if (size != 'S' && size != 'L')
-      throw invalid_argument("Size not one of S, L: " + string(1, size));
+      throw std::invalid_argument(std::string("Size not one of S, L: ") + size);
   }
 
   bool operator<(const Stem& other) const {
@@ -48,14 +48,15 @@ class Stem {
     // Equality if all members are equal
     return species == other.species && size == other.size;
   }
-  static Stem from_string(const string_view spec) {
+  static Stem from_string(const std::string_view spec) {
     if (spec.size() != 2)
-      throw invalid_argument("Species should be initiated from 2-char string.");
+      throw std::invalid_argument(
+          "Species should be initiated from 2-char string.");
     return Stem{spec[0], spec[1]};
   }
 
  private:
-  friend ostream& operator<<(ostream& os, const Stem& stem) {
+  friend std::ostream& operator<<(std::ostream& os, const Stem& stem) {
     // Output streaming for Stem objects
     return (os << stem.species << stem.size);
   }
@@ -63,9 +64,10 @@ class Stem {
 
 namespace std {
 template <>
-struct hash<Stem> {
+struct std::hash<Stem> {
   size_t operator()(const Stem& stem) const {
-    return hash<char>()(stem.species) ^ (hash<char>()(stem.size) << 1);
+    return std::hash<char>()(stem.species) ^
+           (std::hash<char>()(stem.size) << 1);
   }
 };
 }  // namespace std
@@ -77,7 +79,7 @@ class StemCount {
   StemCount(const Stem stem, const int count) : stem(stem), count(count) {}
 
  private:
-  friend ostream& operator<<(ostream& out, StemCount const& req) {
+  friend std::ostream& operator<<(std::ostream& out, const StemCount& req) {
     // Output streaming for StemCount objects
     return out << req.count << req.stem.species;
   }
@@ -87,27 +89,27 @@ class Design {
  public:
   const char name;
   const char size;
-  const forward_list<StemCount> required;
+  const std::forward_list<StemCount> required;
   const int total;
 
   Design(const char name,
          const char size,
-         const forward_list<StemCount> required,
+         const std::forward_list<StemCount> required,
          const int total)
       : name(name), size(size), required(required), total(total) {}
 
-  static Design from_string(const string spec) {
-    regex pat_design{R"(([A-Z])([SL])((?:\d+[a-z])+)(\d+))"};
-    regex pat_spec{R"((\d+)([a-z]))"};
-    smatch match;
-    if (!regex_match(spec, match, pat_design))
-      throw invalid_argument("Not a valid pattern: " + spec);
+  static Design from_string(const std::string spec) {
+    std::regex pat_design{R"(([A-Z])([SL])((?:\d+[a-z])+)(\d+))"};
+    std::regex pat_spec{R"((\d+)([a-z]))"};
+    std::smatch match;
+    if (!std::regex_match(spec, match, pat_design))
+      throw std::invalid_argument(std::string("Not a valid pattern: ") + spec);
     auto name = match[1].first[0];
     auto stem_size = match[2].first[0];
     auto bouquet_size = stoi(match[4]);
 
     // Determine raw maximums per stem species
-    vector<StemCount> raw_stem_specifications;
+    std::vector<StemCount> raw_stem_specifications;
     for (const auto& spec_match : regex_iter_match(match[3], pat_spec)) {
       Stem stem{spec_match[2].first[0], stem_size};
       raw_stem_specifications.push_back({stem, stoi(spec_match[1])});
@@ -116,18 +118,18 @@ class Design {
     // Determine bounded maximums for stem requirements
     int stem_count = raw_stem_specifications.size();
     auto any_stem_max = bouquet_size - stem_count + 1;
-    forward_list<StemCount> required;
+    std::forward_list<StemCount> required;
     for (const auto& spec : raw_stem_specifications) {
-      const auto max_required = min(spec.count, any_stem_max);
+      const auto max_required = std::min(spec.count, any_stem_max);
       if (max_required < 1)
-        throw invalid_argument("Stem count must be a positive int");
-      required.push_front(StemCount{move(spec.stem), max_required});
+        throw std::invalid_argument("Stem count must be a positive int");
+      required.push_front(StemCount{std::move(spec.stem), max_required});
     }
     return Design{name, stem_size, required, bouquet_size};
   }
 
  private:
-  friend ostream& operator<<(ostream& out, Design const& design) {
+  friend std::ostream& operator<<(std::ostream& out, const Design& design) {
     // Output streaming for Design objects
     out << "Design " << design.name << "[" << design.size << "] takes: ";
     for (const auto& req : design.required)
@@ -138,14 +140,14 @@ class Design {
 
 class Bouquet {
  public:
-  Bouquet(const Design& design, const forward_list<StemCount> arrangement)
+  Bouquet(const Design& design, const std::forward_list<StemCount> arrangement)
       : design(design), arrangement(arrangement) {}
 
  private:
   const Design& design;
-  const forward_list<StemCount> arrangement;
+  const std::forward_list<StemCount> arrangement;
 
-  friend ostream& operator<<(ostream& out, Bouquet const& bouquet) {
+  friend std::ostream& operator<<(std::ostream& out, const Bouquet& bouquet) {
     // Output streaming and formatting for Bouquet objects
     out << bouquet.design.name << bouquet.design.size;
     for (const auto& spec : bouquet.arrangement)
@@ -166,29 +168,30 @@ class Composer {
     return stem;
   }
 
-  optional<Bouquet> bouquet_for_stem(const Stem& stem) {
+  std::optional<Bouquet> bouquet_for_stem(const Stem& stem) {
     // Returns a Bouquet if one could be created given a newly inserted stem.
     for (const auto& design : designs[stem]) {
-      forward_list<StemCount> arrangement;
+      std::forward_list<StemCount> arrangement;
       if (_extract(design, arrangement)) {
         return Bouquet{design, arrangement};
       } else {
         _restore(arrangement);
       }
     }
-    return nullopt;
+    return std::nullopt;
   }
 
  private:
-  unordered_map<Stem, int> supply;
-  unordered_map<Stem, forward_list<Design>> designs;
+  std::unordered_map<Stem, int> supply;
+  std::unordered_map<Stem, std::forward_list<Design>> designs;
 
-  bool _extract(const Design& design, forward_list<StemCount>& arrangement) {
+  bool _extract(const Design& design,
+                std::forward_list<StemCount>& arrangement) {
     // Moves flowers from the supply into the given arrangement.
     auto remaining = design.total;
     for (const auto& req : design.required) {
       if (auto& entry = supply[req.stem]) {
-        auto take = min({req.count, entry, remaining});
+        auto take = std::min({req.count, entry, remaining});
         arrangement.push_front({req.stem, take});
         entry -= take;
         remaining -= take;
@@ -199,29 +202,29 @@ class Composer {
     return remaining == 0;
   }
 
-  void _restore(const forward_list<StemCount>& arrangement) {
+  void _restore(const std::forward_list<StemCount>& arrangement) {
     // Adds the stems in the arrangement back to the supply.
     for (const auto& spec : arrangement)
       supply[spec.stem] += spec.count;
   }
 };
 
-bool readline(string& line) {
+bool readline(std::string& line, std::istream& source = std::cin) {
   // Reads a line, signaling the end of a paragraph in addition to EOF.
-  getline(cin, line);
-  return cin && line.size();
+  std::getline(source, line);
+  return source && line.size();
 }
 
 int main() {
-  ios::sync_with_stdio(false);
+  std::ios::sync_with_stdio(false);
   Composer composer;
 
-  for (string line; readline(line);)
+  for (std::string line; readline(line);)
     composer.add_design(Design::from_string(line));
 
-  for (string line; readline(line);) {
+  for (std::string line; readline(line);) {
     auto stem = composer.add_stem(Stem::from_string(line));
     if (auto bouquet = composer.bouquet_for_stem(stem))
-      cout << *bouquet << "\n";
+      std::cout << *bouquet << std::endl;
   }
 }
